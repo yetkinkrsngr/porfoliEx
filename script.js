df2c2d0fd9c6b5b51cc8076dfd88aaafa1eca02d
yetkin karasungur <yetkinkrsngr@gmail.com>
2023-07-21 17:30:32 +0300

    project done

---
+++ script.js
@@ -0,0 +1,42 @@
+const sr = ScrollReveal({
+  distance: "35px",
+  duration: 2400,
+  reset: true,
+});
+sr.reveal(".logo", {
+  delay: 110,
+  origin: "left",
+});
+sr.reveal(".navlist", {
+  delay: 210,
+  origin: "top",
+});
+sr.reveal(".top-btnn", {
+  delay: 310,
+  origin: "right",
+});
+
+sr.reveal(".main-content h4", {
+  delay: 410,
+  origin: "top",
+});
+sr.reveal(".main-content h1", {
+  delay: 510,
+  origin: "left",
+});
+sr.reveal(".main-content p", {
+  delay: 610,
+  origin: "right",
+});
+sr.reveal(".social", {
+  delay: 710,
+  origin: "left",
+});
+sr.reveal(".main-btnn", {
+  delay: 810,
+  origin: "top",
+});
+sr.reveal(".bottom", {
+  delay: 910,
+  origin: "bottom",
+});
